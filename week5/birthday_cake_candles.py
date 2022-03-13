8b90d7ffebd0658659b45cc5b50ad33726fb8cd5
unknown <yoosoobin1997@gmail.com>
2022-03-13 23:27:20 +0900

    week5

---
+++ week5/birthday_cake_candles.py
@@ -0,0 +1,25 @@
+#https://www.hackerrank.com/challenges/birthday-cake-candles/problem?isFullScreen=true
+
+import math
+import os
+import random
+import re
+import sys
+
+
+def birthdayCakeCandles(candles):
+    mx = max(candles)
+    return candles.count(mx)
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    candles_count = int(input().strip())
+
+    candles = list(map(int, input().rstrip().split()))
+
+    result = birthdayCakeCandles(candles)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
